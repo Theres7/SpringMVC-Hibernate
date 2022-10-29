9c84d63d17858d8e63ba1a2399240201f5330391
Theres Paul <113508191+Theres7@users.noreply.github.com>
2022-10-29 16:28:43 +0530

    Update UserDaoImpl.java

--- src/main/java/com/project/dao/UserDaoImpl.java
+++ src/main/java/com/project/dao/UserDaoImpl.java
@@ -1,1 +1,55 @@
+package com.project.dao;
+
+
+import com.project.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @Autowired
+    HibernateTemplate hibernateTemplate;
+
+    // Add or Update User
+    @Override
+    @Transactional
+    public void addOrUpdateUser(User user) {
+        hibernateTemplate.saveOrUpdate( user );
+    }
+
+    // Get All Users
+    @Override
+    public List<User> loadUsers() {
+        return hibernateTemplate.loadAll( User.class );
+    }
+
+    // Get User by Id
+    @Override
+    public User getUserById(int userId) {
+        return hibernateTemplate.get( User.class, userId );
+    }
+
+    // Delete User
+    @Override
+    @Transactional
+    public void deleteUser(int userId) {
+        hibernateTemplate.delete( hibernateTemplate.load( User.class, userId ) );
+    }
+
+   // Add User
+   public void addUser(User user){
+       hibernateTemplate.save( user );
+   }
+
+   // Update User
+   @Transactional
+   public  void updateUser(User user){
+       hibernateTemplate.update( user );
+   }
+}
 

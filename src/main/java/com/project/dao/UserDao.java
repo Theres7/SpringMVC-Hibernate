ce7573a623b9a45b3d8a83e687d805ebd323fb53
Theres Paul <113508191+Theres7@users.noreply.github.com>
2022-10-29 16:23:22 +0530

    Update UserDao.java

--- src/main/java/com/project/dao/UserDao.java
+++ src/main/java/com/project/dao/UserDao.java
@@ -1,1 +1,16 @@
+package com.project.dao;
+
+import com.project.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    void addOrUpdateUser(User user);
+
+    List<User> loadUsers();
+
+    User getUserById(int userId);
+
+    void deleteUser(int userId);
+}
 

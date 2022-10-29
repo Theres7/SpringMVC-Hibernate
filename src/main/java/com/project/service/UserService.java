6208d47239ba1026ec2f4a93d69fc034c6a86d28
Theres Paul <113508191+Theres7@users.noreply.github.com>
2022-10-29 16:40:24 +0530

    Update UserService.java

--- src/main/java/com/project/service/UserService.java
+++ src/main/java/com/project/service/UserService.java
@@ -1,1 +1,16 @@
+package com.project.service;
+
+import com.project.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void createOrUpdate(User user);
+
+    List<User> loadUsers();
+
+    User getUserById(int userId);
+
+    void deleteUser(int userId);
+}
 

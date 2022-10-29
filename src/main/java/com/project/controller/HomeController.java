80a19aee09cd0fb4e5d1e34800a16e424c1217ee
Theres Paul <113508191+Theres7@users.noreply.github.com>
2022-10-29 15:03:15 +0530

    Update HomeController.java

--- src/main/java/com/project/controller/HomeController.java
+++ src/main/java/com/project/controller/HomeController.java
@@ -1,1 +1,25 @@
+package com.project.controller;
+
+import com.project.model.User;
+import com.project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/")
+    public String home(Model model){
+        List<User> userList = service.loadUsers();
+        model.addAttribute( "users",userList );
+        model.addAttribute( "title","User Registration and Login" );
+        return "home";
+    }
+}
 
